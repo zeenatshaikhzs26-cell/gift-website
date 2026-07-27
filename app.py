62edec12dbd8abbe6344cab15bab05bb6941aae1
zeenatshaikhzs26-cell <zeenatshaikhzs2007@gmail.com>
2026-07-27 17:20:56 +0530

    Update app.py

--- app.py
+++ app.py
@@ -14,27 +14,32 @@ def products():
     gift_items = [
         {
             "name": "Hair Clutcher Bouquet ",
-            "price": "₹299",
+            "price": "₹999",
             "image": "https://i.pinimg.com/736x/ff/64/d6/ff64d664b72b73a5d882983ffde95b4f.jpg"
         },
         {
+          "name": "Personalized Photo Magazine",
+          "price": "₹1500",
+          "image": "https://i.pinimg.com/1200x/34/6c/89/346c896935db88c10de250fe2a8763c0.jpg"
+        }
+        {
             "name": "Chocolate Bouquet",
-            "price": "₹599",
+            "price": "₹699",
             "image": "https://i.pinimg.com/736x/df/85/a5/df85a58d70bd8e6252d21af403b05e01.jpg"
         },
         {
             "name": "Custom Bracelet",
-            "price": "₹199",
+            "price": "₹499",
             "image": "https://i.pinimg.com/1200x/f0/2e/0c/f02e0c6ecdad3d8490d53ad72ddc44d4.jpg"
         },
         {
             "name": "DIY Gift Box",
-            "price": "₹499",
+            "price": "₹699",
             "image": "https://i.pinimg.com/736x/ca/ba/b2/cabab209af59895b2e0e9ae1d6fde60a.jpg"
         },
         {
             "name": "Flower Bouquet",
-            "price": "₹399",
+            "price": "₹899",
             "image": "https://i.pinimg.com/736x/19/43/74/194374a135260fe5acbb0f185cc43680.jpg"
         },
         {
@@ -54,17 +59,17 @@ def products():
         },
         {
             "name": "Crochet Flower Bouquet",
-            "price": "₹699",
-            "image": "https://i.pinimg.com/736x/03/56/9c/03569ce36df312c57ead80adc0ff8675.jpg"
+            "price": "₹999",
+            "image": "https://i.pinimg.com/736x/2d/45/a0/2d45a0d2786f5b699409bf1c4f16d036.jpg"
         },
         {
             "name": "Crochet Keychains",
-            "price": "₹149",
+            "price": "₹249",
             "image": "https://i.pinimg.com/736x/12/f6/04/12f604a1a4dde2597aa52dedf19277ef.jpg"
         },
         {
             "name": "Crochet Toys",
-            "price": "₹499",
+            "price": "₹899",
             "image": "https://i.pinimg.com/736x/9e/12/5e/9e125e32bcbdeae07cec10947fdc6b63.jpg"
         }
     ]
